a453b3f3e81f65ec94abaff606ebf91464d448a8
DESKTOP-UD9KGBJ\tungh <tung93pt@gmail.com>
2025-08-17 20:40:08 +0700

    update UI

---
+++ Assets/TungHT/Scripts/SafeAreaPortrait.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(RectTransform))]
+public class SafeAreaPortrait : MonoBehaviour
+{
+    private RectTransform panelSafeArea;
+
+    void Awake()
+    {
+        panelSafeArea = GetComponent<RectTransform>();
+        ApplySafeArea();
+    }
+
+    void ApplySafeArea()
+    {
+        Rect safeArea = Screen.safeArea;
+
+        // Chuyển đổi pixel sang tỉ lệ (anchor)
+        Vector2 anchorMin = safeArea.position;
+        Vector2 anchorMax = safeArea.position + safeArea.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+
+        panelSafeArea.anchorMin = anchorMin;
+        panelSafeArea.anchorMax = anchorMax;
+
+        Debug.Log("SafeArea applied (portrait): " + safeArea);
+    }
+}
